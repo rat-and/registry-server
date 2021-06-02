5dcb6d7c3a47ec2930488b23f53d3bb4e73c7a83
Andre DESKTOP-3HEJK6B <jatar00@onet.eu>
2021-06-02 17:13:29 +0200

    Initial & finall commit

---
+++ src/test/java/edu/springboot/microservices/registryserver/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.springboot.microservices.registryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
